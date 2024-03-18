835a780ca3b8b3cbb6203fb991f1cf931ed51cbf
Lenalicious <100226779+Lenalicious@users.noreply.github.com>
2024-03-18 23:32:48 +0200

    added 18-03-2024 runs
    Uploaded the new measurements

--- src/metriseis_31524.m
+++ src/metriseis_31524.m
@@ -2,6 +2,9 @@ synflood = load('../files/3_15_2024/TCP_SYN_FLOOD.txt')';
 udpflood = load('../files/3_15_2024/UDP_FLOOD_ATTACK.txt')';
 smurf = load('../files/3_15_2024/SMURF_ATTACK.txt')';
 fullattack = load('../files/3_15_2024/20_minutes_attack.txt')';
+attack17_3 = load('../files/3_17_2024/cap1.txt')';
+attack18_3 = load('../files/3_18_2024/cap1_18_3.txt')';
+attack2_18_3 = load('../files/3_18_2024/cap2_18_3.txt')';
 
 Ts = 0.1;
 
@@ -9,6 +12,24 @@ t1 = (0:length(synflood)-1)*Ts/60;
 t2 = (0:length(udpflood)-1)*Ts/60;
 t3 = (0:length(smurf)-1)*Ts/60;
 t4 = (0:length(fullattack)-1)*Ts/60;
+t5 = (0:length(attack17_3)-1)*Ts/60;
+t6 = (0:length(attack18_3)-1)*Ts/60;
+t7 = (0:length(attack2_18_3)-1)*Ts/60;
+
+figure;
+plot(t6,attack18_3);
+title('18/3');
+grid on;
+
+figure;
+plot(t7,attack2_18_3);
+title('18/3 (2)');
+grid on;
+
+figure;
+plot(t5,attack17_3);
+title('17/3');
+grid on;
 
 figure;
 subplot(2,2,1);
@@ -38,3 +59,4 @@ title('20 MINUTES ATTACK AND AFTER');
 ylabel('Temperature (Celcius)');
 xlabel('Time (min)');
 grid on;
+
